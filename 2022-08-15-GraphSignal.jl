9bb12467067856715024a9714d34145f7d7fdff8
cgb2 <miruetoto@gmail.com>
2022-08-15 12:33:03 +0900

    .

---
+++ 2022-08-15-GraphSignal.jl
@@ -0,0 +1,28 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b7cc045e-1c4a-11ed-33d5-97aad6776dc2
+1+1
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═b7cc045e-1c4a-11ed-33d5-97aad6776dc2
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
