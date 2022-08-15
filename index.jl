b9f56ec3c24685e5ed0461be55c689e57b75d933
miruetoto <44726070+miruetoto@users.noreply.github.com>
2022-08-15 03:35:34 +0000

    Deploying to gh-pages from  @ 9bb12467067856715024a9714d34145f7d7fdff8 🚀

---
+++ index.jl
@@ -0,0 +1,30 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9c5d6224-1c4a-11ed-24aa-895284173cfa
+md"""
+# 2022년 줄리아 개인 홈페이지
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─9c5d6224-1c4a-11ed-24aa-895284173cfa
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
